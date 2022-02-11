a5261e2fb3247a50d7c743926f26473a31dce0a8
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2022-02-11 15:27:18 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-#pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ    
-#pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ - TOUCH
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ - TOUCH
 
 hra_zahajena = False
-vysledek_int = 0
-vysledek_str = "X"
 klic = False
-countA = 0
-countB = 0
+vysledek = ["lol"]
 
 def startovac():
-    global hra_zahajena, klic, countA, countB, vysledek_str, vysledek_int
-    countA = 0
-    countB = 0
-    vysledek_int = 0
-    vysledek_str = "X"
+    global hra_zahajena, klic, vysledek
     basic.clear_screen()
     klic = True
+    vysledek = []
     soundExpression.happy.play()
     nahodna_doba = randint(3000, 10000)
     basic.pause(nahodna_doba)
@@ -24,16 +18,16 @@ def startovac():
 control.in_background(startovac)
 
 def on_forever():
-    global hra_zahajena, vysledek_int, vysledek_str, klic, countA, countB
+    global hra_zahajena, klic, vysledek
     
-    pin1 = pins.analog_read_pin(AnalogPin.P1)  # - ANALOG
-    pin2 = pins.analog_read_pin(AnalogPin.P2)  # - ANALOG
+    #pin1 = pins.analog_read_pin(AnalogPin.P1)  # - ANALOG
+    #pin2 = pins.analog_read_pin(AnalogPin.P2)  # - ANALOG
 
-    #is_pin1 = input.pin_is_pressed(TouchPin.P1) # - TOUCH
-    #is_pin2 = input.pin_is_pressed(TouchPin.P2) # - TOUCH
+    is_pin1 = input.pin_is_pressed(TouchPin.P1) # - TOUCH
+    is_pin2 = input.pin_is_pressed(TouchPin.P2) # - TOUCH
 
-    is_pin1 = pin1 < 500  # - ANALOG
-    is_pin2 = pin2 < 500  # - ANALOG
+    #is_pin1 = pin1 < 500  # - ANALOG
+    #is_pin2 = pin2 < 500  # - ANALOG
 
     #is_pin1 = pins.digital_read_pin(DigitalPin.P1) == 0 # - DIGITÁLNÍ
     #is_pin2 = pins.digital_read_pin(DigitalPin.P2) == 0 # - DIGITÁLNÍ
@@ -41,48 +35,46 @@ def on_forever():
     console.log_value("is_piN1", is_pin1)
     console.log_value("is_piN2", is_pin2)
     
-    console.log_value("pin1", pin1) # - ANALOG
-    console.log_value("pin2", pin2) # - ANALOG
+    #console.log_value("pin1", pin1) # - ANALOG
+    #console.log_value("pin2", pin2) # - ANALOG
 
     if hra_zahajena and klic:
         if is_pin1  and is_pin2:
-            vysledek_str = "R"
+            vysledek.append("R")
             klic = False
         if is_pin1:
-            vysledek_int = 1
+            vysledek.append("1")
             klic = False
         if is_pin2:
-            vysledek_int = 2
+            vysledek.append("2")
             klic = False
-        
     else:
         if is_pin1:
-            countA =+ 1
+            vysledek.append("B")
         if is_pin2:
-            countB =+ 1
-            
-    vysledek()
+            vysledek.append("A")
+    vysledek_hry()
 basic.forever(on_forever)
 
-def vysledek():
-    global hra_zahajena, klic, vysledek_int, vysledek_str, countA, countB
+def vysledek_hry():
+    global hra_zahajena, klic, vysledek
     splneno = False
-    if vysledek_str == "R":
+    if "R" in vysledek:
         basic.show_string("R")
         splneno = True
-    elif vysledek_int == 1:
+    elif "1" in vysledek:
         basic.show_number(1)
         splneno = True
-    elif vysledek_int == 2:
+    elif "2" in vysledek:
         basic.show_number(2)
         splneno = True
-    elif countB > 0 and countA > 0 and hra_zahajena and klic:
+    elif "A" in vysledek and "B" in vysledek and hra_zahajena and klic:
         basic.show_string("C")
         splneno = True
-    elif countB > 0 and countA == 0 and hra_zahajena and klic:
+    elif "A" in vysledek and hra_zahajena and klic:
         basic.show_string("A")
         splneno = True
-    elif countA > 0 and countB == 0 and hra_zahajena and klic:
+    elif "B" in vysledek and hra_zahajena and klic:
         basic.show_string("B")
         splneno = True
 
@@ -90,4 +82,3 @@ def vysledek():
         hra_zahajena = False
         basic.pause(3000)
         control.in_background(startovac)
-
--- main.ts
+++ main.ts
@@ -1,19 +1,15 @@
-// pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ    
-// pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE) # - KAPACITNÍ
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+//  - KAPACITNÍ - TOUCH
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+//  - KAPACITNÍ - TOUCH
 let hra_zahajena = false
-let vysledek_int = 0
-let vysledek_str = "X"
 let klic = false
-let countA = 0
-let countB = 0
+let vysledek = ["lol"]
 function startovac() {
     
-    countA = 0
-    countB = 0
-    vysledek_int = 0
-    vysledek_str = "X"
     basic.clearScreen()
     klic = true
+    vysledek = []
     soundExpression.happy.play()
     let nahodna_doba = randint(3000, 10000)
     basic.pause(nahodna_doba)
@@ -24,72 +20,68 @@ function startovac() {
 control.inBackground(startovac)
 basic.forever(function on_forever() {
     
-    let pin1 = pins.analogReadPin(AnalogPin.P1)
-    //  - ANALOG
-    let pin2 = pins.analogReadPin(AnalogPin.P2)
-    //  - ANALOG
-    // is_pin1 = input.pin_is_pressed(TouchPin.P1) # - TOUCH
-    // is_pin2 = input.pin_is_pressed(TouchPin.P2) # - TOUCH
-    let is_pin1 = pin1 < 500
-    //  - ANALOG
-    let is_pin2 = pin2 < 500
-    //  - ANALOG
+    // pin1 = pins.analog_read_pin(AnalogPin.P1)  # - ANALOG
+    // pin2 = pins.analog_read_pin(AnalogPin.P2)  # - ANALOG
+    let is_pin1 = input.pinIsPressed(TouchPin.P1)
+    //  - TOUCH
+    let is_pin2 = input.pinIsPressed(TouchPin.P2)
+    //  - TOUCH
+    // is_pin1 = pin1 < 500  # - ANALOG
+    // is_pin2 = pin2 < 500  # - ANALOG
     // is_pin1 = pins.digital_read_pin(DigitalPin.P1) == 0 # - DIGITÁLNÍ
     // is_pin2 = pins.digital_read_pin(DigitalPin.P2) == 0 # - DIGITÁLNÍ
     console.logValue("is_piN1", is_pin1)
     console.logValue("is_piN2", is_pin2)
-    console.logValue("pin1", pin1)
-    //  - ANALOG
-    console.logValue("pin2", pin2)
-    //  - ANALOG
+    // console.log_value("pin1", pin1) # - ANALOG
+    // console.log_value("pin2", pin2) # - ANALOG
     if (hra_zahajena && klic) {
         if (is_pin1 && is_pin2) {
-            vysledek_str = "R"
+            vysledek.push("R")
             klic = false
         }
         
         if (is_pin1) {
-            vysledek_int = 1
+            vysledek.push("1")
             klic = false
         }
         
         if (is_pin2) {
-            vysledek_int = 2
+            vysledek.push("2")
             klic = false
         }
         
     } else {
         if (is_pin1) {
-            countA = +1
+            vysledek.push("B")
         }
         
         if (is_pin2) {
-            countB = +1
+            vysledek.push("A")
         }
         
     }
     
-    vysledek()
+    vysledek_hry()
 })
-function vysledek() {
+function vysledek_hry() {
     
     let splneno = false
-    if (vysledek_str == "R") {
+    if (vysledek.indexOf("R") >= 0) {
         basic.showString("R")
         splneno = true
-    } else if (vysledek_int == 1) {
+    } else if (vysledek.indexOf("1") >= 0) {
         basic.showNumber(1)
         splneno = true
-    } else if (vysledek_int == 2) {
+    } else if (vysledek.indexOf("2") >= 0) {
         basic.showNumber(2)
         splneno = true
-    } else if (countB > 0 && countA > 0 && hra_zahajena && klic) {
+    } else if (vysledek.indexOf("A") >= 0 && vysledek.indexOf("B") >= 0 && hra_zahajena && klic) {
         basic.showString("C")
         splneno = true
-    } else if (countB > 0 && countA == 0 && hra_zahajena && klic) {
+    } else if (vysledek.indexOf("A") >= 0 && hra_zahajena && klic) {
         basic.showString("A")
         splneno = true
-    } else if (countA > 0 && countB == 0 && hra_zahajena && klic) {
+    } else if (vysledek.indexOf("B") >= 0 && hra_zahajena && klic) {
         basic.showString("B")
         splneno = true
     }
